4b0d9b31173b1e2168c025871a9f0f69e70afe06
Victor Naranjo <vienteck@protonmail.com>
2023-12-07 16:34:42 -0800

    Update main.go

--- day3/main.go
+++ day3/main.go
@@ -22,39 +22,95 @@ func main() {
 		row := scanner.Text()
 		lineText = append(lineText, row)
 	}
-
+	result := 0
 	startOfNumber := ""
+	addNumber := false
 	for j := 0; j < len(lineText); j++ {
 		for i := 0; i < len(lineText[j]); i++ {
-			addNumber := true
-			if string(lineText[j][i]) > "0" && string(lineText[j][i]) < "9" {
+			if string(lineText[j][i]) >= "0" && string(lineText[j][i]) <= "9" {
 				if startOfNumber == "" {
+					addNumber = false
 					//check above
 					if j > 0 {
-						if string(lineText[j-1][i]) == "." {
-							addNumber = false
+						if string(lineText[j-1][i]) != "." {
+							addNumber = true
 						}
 					}
 					//check left
 					if i > 0 {
-						addNumber = false
-						fmt.Println(addNumber)
+						if string(lineText[j][i-1]) != "." {
+							addNumber = true
+						}
+					}
+					//check upper-left
+					if j > 0 && i > 0 {
+						if string(lineText[j-1][i-1]) != "." {
+							addNumber = true
+						}
+					}
+					//check down
+					if j < len(lineText)-1 {
+						if string(lineText[j+1][i]) != "." {
+							addNumber = true
+						}
+					}
+					//check lower-left
+					if j < len(lineText)-1 && i > 0 {
+						if string(lineText[j+1][i-1]) != "." {
+							addNumber = true
+						}
 					}
 					//check left, up-left and down-left
 				} else {
-					//check up and down
+					//check down
+					if j < len(lineText)-1 {
+						if string(lineText[j+1][i]) != "." {
+							addNumber = true
+						}
+					}
+					//check above
+					if j > 0 {
+						if string(lineText[j-1][i]) != "." {
+							addNumber = true
+						}
+					}
 				}
 
 				startOfNumber = fmt.Sprintf("%v%v", startOfNumber, string(lineText[j][i]))
 			} else {
 				if startOfNumber != "" {
-					//process number
 					number, _ := strconv.Atoi(startOfNumber)
-					fmt.Println(number)
+					//check down
+					if j < len(lineText)-1 {
+						if string(lineText[j+1][i]) != "." {
+							addNumber = true
+						}
+					}
+					//check above
+					if j > 0 {
+						if string(lineText[j-1][i]) != "." {
+							addNumber = true
+						}
+					}
+					if string(lineText[j][i]) != "." {
+						addNumber = true
+					}
+					if addNumber {
+						result += number
+					} else {
+						fmt.Printf("Not printing nubmer %v line %v\n", number, j)
+					}
 					startOfNumber = ""
 				}
 			}
-		}
-	}
 
+			if i == len(lineText[j])-1 {
+				if addNumber == true {
+					number, _ := strconv.Atoi(startOfNumber)
+					result += number
+				}
+			}
+		}
+		fmt.Println(result)
+	}
 }
